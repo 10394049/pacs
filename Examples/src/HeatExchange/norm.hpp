25853d15284dbc6e7a28600f8a0426364f2cac51
10394049 <danielecarlo.corti@mail.polimi.it>
2016-04-17 15:28:42 +0200

    Challenge_1.3

--- Examples/src/HeatExchange/norm.hpp
+++ Examples/src/HeatExchange/norm.hpp
@@ -1,18 +1,19 @@
 double norm (std::string Norm, double xn, double xo, double h, double xpsn, double xpso){
-	// Rn norm
+	// Rn
 	if (Norm.compare("Rn")==0){
 		return (xn-xo)*(xn-xo);
 	}
 	
 	else {
-		// L2 norm
+		// L2
 		if (Norm.compare("L2")==0){
-			return(xn-xo)*(xn-xo)*h;
+			//return(xn-xo)*(xn-xo)*h;
+			return(h/6 * ( (xo-xpso)^2 + 4 *( (xn-xpsn)/2-(xo-xpso)/2 )^2 + (xn-xpsn)^2 );
 		}
 		else {
-		// H1 norm
+		// H1
 			if (Norm.compare("H1")==0){
-				return ((xn-xo)*(xn-xo)*h) + (((xn-xpsn)-(xo-xpso))^2/h);
+				return (h/6 * ( (xo-xpso)^2 + 4 * ( (xn-xpsn)/2-(xo-xpso)/2 )^2 + (xn-xpsn)^2  + (((xn-xpsn)-(xo-xpso))^2/h);
 			}
 		}
 	}
